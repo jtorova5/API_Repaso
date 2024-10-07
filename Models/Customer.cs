89fadb3da87a95f0c617260ccb0935bf6c65dd99
jtorova5 <jtorova5@gmail.com>
2024-10-06 19:01:19 -0500

    feat: adding customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RepasoAPI.Models;
+
+[Table("customer")]
+public class Customer(string userName, string email, string password)
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    [Column("name")]
+    public required string UserName { get; set; } = userName.ToLower().Trim();
+
+    [MaxLength(100)]
+    [Column("email")]
+    [EmailAddress]
+    public required string Email { get; set; } = email.ToLower().Trim();
+
+    [MaxLength(256)]
+    [Column("password")]
+    public required string Password { get; set; } = password.ToLower().Trim();
+}
