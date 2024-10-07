067d03ccb19da838b7bdb610d8e990b82d295f52
jtorova5 <jtorova5@gmail.com>
2024-10-06 20:22:58 -0500

    feat: adding IProductRepository

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using RepasoAPI.Models;
+
+namespace RepasoAPI.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllProducts();
+    Task<Product> GetProductById(int id);
+    Task AddProduct(Product product);
+    Task UpdateProduct(Product product);
+    Task DeleteProduct(int id);
+}
